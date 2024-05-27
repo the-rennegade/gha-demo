f411a25aed6f194a87a390d06dbfc7a9d761583b
Ryan Renn <ryan.renn@greyshore.com>
2024-05-27 18:28:03 -0400

    create matrix wf 2

--- example.py
+++ example.py
@@ -1,3 +1,5 @@
+import sys
+
 def add(a, b):
     """Add two numbers."""
     return a + b
@@ -5,3 +7,11 @@ def add(a, b):
 def subtract(a, b):
     """Subtract two numbers."""
     return a - b
+
+print(f"Python Version: {sys.version}")
+
+print("3 + 2 =")
+print(add(3, 2))
+
+print("5 - 2 =")
+print(subtract(5, 2))
--- test-example.py
+++
@@ -1,9 +0,0 @@
-from example import add, subtract
-
-def test_add():
-    assert add(1, 2) == 3
-    assert add(5, 7) == 12
-
-def test_subtract():
-    assert subtract(5, 2) == 3
-    assert subtract(10, 7) == 3
